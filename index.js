50b16772bd2370c334fb8cd98fa5ee1120476e96
Joseph Parker <jparker010190@gmail.com>
2018-07-06 23:37:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ var newID = oldID + 1000000000
 var ageIsValid = Number.isInteger(currentAge);
 var randomNumber = Math.random();
 var randomInteger = Math.round(randomNumber);
-var randomUserID = randomNumber.floor(randomInteger * 1000000001 )
+var randomUserID = Math.floor(randomInteger * 10 )
